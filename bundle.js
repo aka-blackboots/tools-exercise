97017bb21d784f719f42e3ead75db5658e094e77
RSR <rodrigo.sanchezdelrio.imbim@gmail.com>
2022-11-11 07:36:35 -0300

    Ajustes varios 2

--- bundle.js
+++ bundle.js
@@ -729,7 +729,7 @@ const proyectos = [
     {
         name: "Proyecto IFC.js 1",
         id:"1",
-        url: "./s0830-1-mchh.ifc"
+        url: "s0830-1-mchh.ifc"
     },
     {
         name: "Proyecto IFC.js  2",
--- proyectos.js
+++ proyectos.js
@@ -2,7 +2,7 @@ export const proyectos = [
     {
         name: "Proyecto IFC.js 1",
         id:"1",
-        url: "./s0830-1-mchh.ifc"
+        url: "s0830-1-mchh.ifc"
     },
     {
         name: "Proyecto IFC.js  2",
