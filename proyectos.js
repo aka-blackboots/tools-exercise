9b3fc51340a4efb6c52b556acc8a274734ef469d
RSR <rodrigo.sanchezdelrio.imbim@gmail.com>
2022-11-11 06:22:39 -0300

    Ubicación proyecto N°1

--- proyectos.js
+++ proyectos.js
@@ -2,7 +2,7 @@ export const proyectos = [
     {
         name: "Proyecto IFC.js 1",
         id:"1",
-        url: "https://ifcjs.github.io/ifcjs-crash-course/sample-apps/01/"
+        url: "./s0830-1-mchh.ifc"
     },
     {
         name: "Proyecto IFC.js  2",
