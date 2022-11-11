e3bfacb55b120cf962e6a7a026a5366b99db98d9
RSR <rodrigo.sanchezdelrio.imbim@gmail.com>
2022-11-11 07:43:10 -0300

    Reducción a un proyecto

--- bundle.js
+++ bundle.js
@@ -731,26 +731,7 @@ const proyectos = [
         id:"1",
         url: "s0830-1-mchh.ifc"
     },
-    {
-        name: "Proyecto IFC.js  2",
-        id:"2",
-        url: "https://ifcjs.github.io/ifcjs-crash-course/sample-apps/02/"
-    },
-    {
-        name: "Proyecto IFC.js  3",
-        id:"3",
-        url: "https://ifcjs.github.io/ifcjs-crash-course/sample-apps/03/"
-    },
-    {
-        name: "Proyecto IFC.js  4",
-        id:"4",
-        url: "https://ifcjs.github.io/ifcjs-crash-course/sample-apps/04/"
-    },
-    {
-        name: "Proyecto IFC.js  5",
-        id:"5",
-        url: "https://ifcjs.github.io/ifcjs-crash-course/sample-apps/05/"
-    }
+
 ];
 
 const container = document.getElementById('viewer-container');
